1b398773c08008e77334cfde9ecf7d82620e45c2
Shilpa Patwary <shipatwa@publicisgroupe.net>
2018-10-26 16:24:16 +0530

    updating response for default route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res) => {
-    res.send("Hello, Heroku rew");
+    res.send("Hello, Heroku");
 });
 
 app.get("/echo/:name",(req, res) => {
