ac9b2063f619003e5775121e82893ff29def9c67
Shilpa Patwary <shipatwa@publicisgroupe.net>
2018-10-26 10:29:52 +0530

    modify response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res) => {
-    res.send("Hello, Heroku try1");
+    res.send("Hello, Heroku try2");
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
