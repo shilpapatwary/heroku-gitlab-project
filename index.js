1ffe6e80b695128d99a2155b04efcfdaa9b4fa18
Shilpa Patwary <shipatwa@publicisgroupe.net>
2018-10-26 10:14:39 +0530

    changing world to heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res) => {
-    res.send("Hello, World");
+    res.send("Hello, Heroku");
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
